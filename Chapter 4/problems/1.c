d82e73f4cdc83dfbb3b78e4bf9f6a53312bec34a
ayushdontcode <ayushtiwari0123456@gmail.com>
2025-07-13 00:55:03 +0530

    example 1

--- Chapter 4/problems/1.c
+++ Chapter 4/problems/1.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
 int main(){
-    printf("hello github");
-    return 0;
+    float m1,m2,m3,m4,m5;
+    printf("enter marks in all subjects; \n");
+    scanf("%f %f %f %f %f", &m1,&m2,&m3,&m4,&m5);
+    float total_score=m1+m2+m3+m4+m5;
+    float percentage = 500/total_score*100;
+    if(percentage>=60){
+        printf("first division");
+    }
+    else if(percentage<60 || percentage>=50){
+        printf("second devision");
+    }
+    else if(percentage<50 || percentage>=40){
+        printf("third devision");
+    }
+    else {
+        printf("fail");
+    }
 }
