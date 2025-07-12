32274217e5a51a2d2f422506bc94444659df0a7a
ayushdontcode <ayushtiwari0123456@gmail.com>
2025-07-13 01:14:37 +0530

    actiom

--- Chapter 4/problems/1.c
+++ Chapter 4/problems/1.c
@@ -1,24 +1,34 @@
-<<<<<<< HEAD
-#include <stdio.h>
-
 int main(){
-    float m1,m2,m3,m4,m5;
-    printf("enter marks in all subjects; \n");
-    scanf("%f %f %f %f %f", &m1,&m2,&m3,&m4,&m5);
-    float total_score=m1+m2+m3+m4+m5;
-    float percentage = 500/total_score*100;
-    if(percentage>=60){
-        printf("first division");
+    int year;
+    int leap_years, ordinary_years, odd_days;
+
+    printf("Enter the year: ");
+    scanf("%d", &year);
+
+    // Count number of leap years from year 1 to year-1
+    leap_years = (year - 1) / 4 - (year - 1) / 100 + (year - 1) / 400;
+
+    // Count number of ordinary years
+    ordinary_years = (year - 1) - leap_years;
+
+    // Calculate total odd days
+    odd_days = (ordinary_years * 1 + leap_years * 2) % 7;
+
+    // Print day using if-else
+    printf("On 1st January %d it was: ", year);
+    if(odd_days == 0){
+        printf("Monday\n");
     }
-    else if(percentage<60 || percentage>=50){
-        printf("second devision");
+    else if(odd_days == 1){
+        printf("Tuesday\n");
     }
-    else if(percentage<50 || percentage>=40){
-        printf("third devision");
+    else if(odd_days == 2){
+        printf("Wednesday\n");    }    else if(odd_days == 3){        printf("Thursday\n");    }    else if(odd_days == 4){        printf("Friday\n");    }    else if(odd_days == 5){
+        printf("Saturday\n");
     }
-    else {
-        printf("fail");
+    else if(odd_days == 6){
+       printf("Sunday\n");
     }
+
+    return 0;
 }
-=======
->>>>>>> b74454f672321139a6a8674c961ba4e9fb0d4d28
